f0ff5fbde7b8856827d1b3819cb0ff0e65b94975
Bealby <mgbealby@gmail.com>
2021-03-07 11:51:30 +0000

    Fixed quiz js to show score total

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -1,136 +1,141 @@
 var quiz = {
-  // (A) PROPERTIES 
-  // (A1) QUESTIONS & ANSWERS
-  // Q = QUESTION, O = OPTIONS, A = CORRECT ANSWER
-  data: [
-  {
-    q : "In which country is St. Patrick the patron saint?",
-    o : [
-      "Scotland",
-      "Germany",
-      "Ireland",
-      "Iceland"
+    // (A) PROPERTIES 
+    // (A1) QUESTIONS & ANSWERS
+    // Q = QUESTION, O = OPTIONS, A = CORRECT ANSWER
+    data: [
+        {
+            q: "In which country is St. Patrick the patron saint?",
+            o: [
+                "Scotland",
+                "Germany",
+                "Ireland",
+                "Iceland"
+            ],
+            a: 2
+        },
+        {
+            q: "What animal did St. Patrick drive out of Ireland?",
+            o: [
+                "snake",
+                "Scorpion",
+                "Spider",
+                "Rat"
+            ],
+            a: 0
+        },
+        {
+            q: "What happens if you don't wear Green on St. Patrick's Day ?",
+            o: [
+                "Pinched",
+                "Kissed",
+                "Punched",
+                "Bad-Luck"
+            ],
+            a: 3
+        },
+        {
+            q: "How many leaves does a shamrock have?",
+            o: [
+                "One Leaf",
+                "Two Leaves",
+                "Three Leaves",
+                "Four Leaves"
+            ],
+            a: 3
+        },
+        {
+            q: "What colour was originally associated with St. Patrick's day?",
+            o: [
+                "Yellow",
+                "Green",
+                "Purple",
+                "Blue"
+            ],
+            a: 1
+        }
     ],
-    a : 2 
-  },
-  {
-    q : "What animal did St. Patrick drive out of Ireland?",
-    o : [
-      "snake",
-      "Scorpion",
-      "Spider",
-      "Rat"
-    ],
-    a : 0
-  },
-  {
-    q : "What happens if you don't wear Green on St. Patrick's Day ?",
-    o : [
-      "Pinched",
-      "Kissed",
-      "Punched",
-      "Bad-Luck"
-    ],
-    a : 3
-  },
-  {
-    q : "How many leaves does a shamrock have?",
-    o : [
-      "One Leaf",
-      "Two Leaves",
-      "Three Leaves",
-      "Four Leaves"
-    ],
-    a : 3
-  },
-  {
-    q : "What colour was originally associated with St. Patrick's day?",
-    o : [
-      "Yellow",
-      "Green",
-      "Purple",
-      "Blue"
-    ],
-    a : 1
-  }
-  ],
 
-  // (A2) HTML ELEMENTS
-  hWrap: null, // HTML quiz container
-  hQn: null, // HTML question wrapper
-  hAns: null, // HTML answers wrapper
+    // (A2) HTML ELEMENTS
+    hWrap: null, // HTML quiz container
+    hQn: null, // HTML question wrapper
+    hAns: null, // HTML answers wrapper
 
-  // (A3) GAME FLAGS
-  now: 0, // current question
-  score: 0, // current score
+    // (A3) GAME FLAGS
+    now: 0, // current question
+    score: 0, // current score
 
-  // (B) INIT QUIZ HTML
-  init: function(){
-    // (B1) WRAPPER
-    quiz.hWrap = document.getElementById("quizWrap");
+    // (B) INIT QUIZ HTML
+    init: function () {
+        // (B1) WRAPPER
+        quiz.hWrap = document.getElementById("quizWrap");
 
-    // (B2) QUESTIONS SECTION
-    quiz.hQn = document.createElement("div");
-    quiz.hQn.id = "quizQn";
-    quiz.hWrap.appendChild(quiz.hQn);
+        // (B2) QUESTIONS SECTION
+        quiz.hQn = document.createElement("div");
+        quiz.hQn.id = "quizQn";
+        quiz.hWrap.appendChild(quiz.hQn);
 
-    // (B3) ANSWERS SECTION
-    quiz.hAns = document.createElement("div");
-    quiz.hAns.id = "quizAns";
-    quiz.hWrap.appendChild(quiz.hAns);
+        // (B3) ANSWERS SECTION
+        quiz.hAns = document.createElement("div");
+        quiz.hAns.id = "quizAns";
+        quiz.hWrap.appendChild(quiz.hAns);
 
-    // (B4) GO!
-    quiz.draw();
-  },
+        // (B4) GO!
+        quiz.draw();
+    },
 
-  // (C) DRAW QUESTION
-  draw: function(){
-    // (C1) QUESTION
-    quiz.hQn.innerHTML = quiz.data[quiz.now].q;
+    // (C) DRAW QUESTION
+    draw: function () {
+        // (C1) QUESTION
+        quiz.hQn.innerHTML = quiz.data[quiz.now].q;
 
-    // (C2) OPTIONS
-    quiz.hAns.innerHTML = "";
-    for (let i in quiz.data[quiz.now].o) {
-      let radio = document.createElement("input");
-      radio.type = "radio";
-      radio.name = "quiz";
-      radio.id = "quizo" + i;
-      quiz.hAns.appendChild(radio);
-      let label = document.createElement("label");
-      label.innerHTML = quiz.data[quiz.now].o[i];
-      label.setAttribute("for", "quizo" + i);
-      label.dataset.idx = i;
-      label.addEventListener("click", quiz.select);
-      quiz.hAns.appendChild(label);
-    }
-  },
-  
-  // (D) OPTION SELECTED
-  select: function(){
-    // (D1) DETACH ALL ONCLICK
-    let all = quiz.hAns.getElementsByTagName("label");
-    for (let label of all) {
-      label.removeEventListener("click", quiz.select);
-    }
-
-    // (D2) CHECK IF CORRECT
-    let correct = this.dataset.idx == quiz.data[quiz.now].a;
-    if (correct) { 
-      quiz.score++; 
-      this.classList.add("correct");
-    } else {
-      this.classList.add("wrong");
-    }
-  
-    // (D3) NEXT QUESTION OR END GAME
-    quiz.now++;
-    setTimeout(function(){
-      if (quiz.now < quiz.data.length) { quiz.draw(); } 
-      else {
-        quiz.hQn.innerHTML = `You total score is ${quiz.score}`;
+        // (C2) OPTIONS
         quiz.hAns.innerHTML = "";
-      }
-    }, 1000);
-  }
+        for (let i in quiz.data[quiz.now].o) {
+            let radio = document.createElement("input");
+            radio.type = "radio";
+            radio.name = "quiz";
+            radio.id = "quizo" + i;
+            quiz.hAns.appendChild(radio);
+            let label = document.createElement("label");
+            label.innerHTML = quiz.data[quiz.now].o[i];
+            label.setAttribute("for", "quizo" + i);
+            label.dataset.idx = i;
+            label.addEventListener("click", quiz.select);
+            quiz.hAns.appendChild(label);
+        }
+    },
+
+    // (D) OPTION SELECTED
+    select: function () {
+        // (D1) DETACH ALL ONCLICK
+        let all = quiz.hAns.getElementsByTagName("label");
+        for (let label of all) {
+            label.removeEventListener("click", quiz.select);
+        }
+
+        // (D2) CHECK IF CORRECT
+        let correct = this.dataset.idx == quiz.data[quiz.now].a;
+        if (correct) {
+            quiz.score++;
+            this.classList.add("correct");
+        } else {
+            this.classList.add("wrong");
+        }
+
+        // (D3) NEXT QUESTION OR END GAME
+        quiz.now++;
+        setTimeout(function () {
+            if (quiz.now < quiz.data.length) { quiz.draw(); }
+            else {
+                quiz.hQn.innerHTML = `You total score is ${quiz.score}`;
+                quiz.hAns.innerHTML = "";
+                let totalScore = document.querySelector('#score');
+
+                totalScore.value = quiz.score;
+            }
+
+        }, 1000);
+    }
 };
+
 window.addEventListener("load", quiz.init);
